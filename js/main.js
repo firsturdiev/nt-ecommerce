f09e3f4b3d0d5e3f77cb70a1abff52333493b013
Samandar Turdiev <firsturdiev@gmail.com>
2021-07-30 12:32:19 +0500

    Hech narsa qilinmadi

--- js/main.js
+++ js/main.js
@@ -7,3 +7,17 @@ elAccordionToggle.forEach(toggle => {
     toggle.closest('.footer-nav__item').classList.toggle('footer-nav__item--open');
   })
 })
+
+// menu
+
+let elMenuOpen = document.querySelector('.js-menu-open');
+let elMenuClose = document.querySelector('.categories__close');
+let elMenu = document.querySelector('.categories');
+
+elMenuOpen.addEventListener('click', () => {
+  elMenu.classList.add('categories--open');
+})
+
+elMenuClose.addEventListener('click', () => {
+  elMenu.classList.remove('categories--open');
+})
